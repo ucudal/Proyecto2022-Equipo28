eecf96dc1d6d5ca71379668c0a469893fb10a33f
N4choCorrea <104398247+N4choCorrea@users.noreply.github.com>
2022-09-30 19:39:38 -0300

    Add files via upload

---
+++ primeraparte/src/Library/Empleador.cs
@@ -0,0 +1,36 @@
+﻿namespace Library;
+public class Empleador
+{
+    public Empleador(string informacion_contacto, string reputacion)
+    {
+        this.Info=informacion_contacto;
+        this.Reputacion=reputacion;
+    }
+    public string Info { get; set; } //Informacion de contacto
+    public string Reputacion { get; set; } //Reputacion del empleador
+    public void Registro()
+    {
+        //Registrarme en la plataforma
+        //Proveer informacion de contacto
+    }
+    public void Buscar_ofertas()
+    {
+        //Tener la capacidad de buscar ofertas
+        //Tener la capacidad de filtrarlas por categoria
+        //Tener la capacidad de filtrarlas por ubicacion
+        //Tener la capacidad de filtrarlas por reputacion
+    }
+    public void Contactar_trabajador()
+    {
+        //Contactar a un trabajador
+        //Poder enviarle un mensaje
+        //Para tener los servicios determinados que necesito
+    }
+    public void Calificar_trabajador()
+    {
+        //Poder calificar a un trabajador
+        //Despues de un mes deben calificar sino sera neutral
+        //Conocer la reputacion del trabajador
+    }
+
+}
